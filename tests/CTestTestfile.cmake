dfe553f12bb0ef2b64073362b80a02f45b0b28b6
Nikpop228 <nikipet15.06@gmail.com>
2025-05-26 02:42:19 +0300

    yml cmakelists

---
+++ tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/Nikpop228/workspace/projects/tmp-lab05/tests
+# Build directory: /home/Nikpop228/workspace/projects/tmp-lab05/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test "tests")
+set_tests_properties(test PROPERTIES  _BACKTRACE_TRIPLES "/home/Nikpop228/workspace/projects/tmp-lab05/tests/CMakeLists.txt;18;add_test;/home/Nikpop228/workspace/projects/tmp-lab05/tests/CMakeLists.txt;0;")
